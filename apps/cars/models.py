f0d43637e05676162bb16a574d544e4e0af8087d
VeTaL2022 <vetal.kots2003@gmail.com>
2022-12-08 15:49:51 +0200

    initial commit

--- apps/cars/models.py
+++ apps/cars/models.py
@@ -9,7 +9,7 @@ class CarModel(models.Model):
         db_table = 'cars'
 
     brand = models.CharField(max_length=15, unique=True, validators=[
-        V.MinValueValidator(3), V.MaxValueValidator(15)
+        V.MinLengthValidator(3), V.MaxLengthValidator(15)
     ])
     grad_year = models.IntegerField(default=2000)
     n_seats = models.IntegerField()
